d30c6d5767fcd5a19d76da63ea69be0d04d24a0b
Maria Jose <cg.mariajose492@gmail.com>
2024-03-14 16:48:13 -0500

    Open the port of express

---
+++ index.js
@@ -0,0 +1,11 @@
+// Importación del framework express para realizar solicitudes http 
+const express = require('express');
+const app = express();
+
+// poder acceder a leer el archivo .env 
+require("dotenv").config();
+const CONNECTION_PORT = process.env.PORT || 3005;
+
+app.listen(CONNECTION_PORT, () => {
+    console.log(`Using the port ${CONNECTION_PORT}`);
+})
