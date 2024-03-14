41d9c83dddbf05d425c222bf4feda23bd71c1ba9
Maria Jose <cg.mariajose492@gmail.com>
2024-03-14 17:34:21 -0500

    Connection to the database and creation of user schema 2.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Importación del framework express para realizar solicitudes http 
 const express = require('express');
+const mongoose = require("mongoose");
+const cors = require("cors")
 const app = express();
 
 // poder acceder a leer el archivo .env 
@@ -9,3 +11,19 @@ const CONNECTION_PORT = process.env.PORT || 3005;
 app.listen(CONNECTION_PORT, () => {
     console.log(`Using the port ${CONNECTION_PORT}`);
 })
+
+// Conexión a la base de datos 
+const mongo_connect = () => {
+    try {
+        mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
+            .then(() => {
+                console.log("Connected to database");
+            })
+            .catch((err) => {
+                console.log("Error connecting to the database");
+            });
+
+    } catch (error) {
+
+    }
+}
